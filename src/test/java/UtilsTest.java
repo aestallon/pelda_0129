27e1160f3ccf7535f4ce9e8ea21bc183934652d7
Papp Szabolcs <papp.szabolcs.bazil@gmail.com>
2022-06-11 16:11:39 +0200

    Add unit tests for Utils#maxker(int)

--- src/test/java/UtilsTest.java
+++ src/test/java/UtilsTest.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class UtilsTest {
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UtilsTest {
+
+    @Test
+    public void emptyArraysMax_ShouldBeMinus1() {
+        int[] array = new int[0];
+        assertEquals(-1, Utils.maxker(array));
+    }
+
+    @Test
+    public void OneLengthArray_Gives0() {
+        int[] array = { 25 };
+        assertEquals(0, Utils.maxker(array));
+    }
+
+    @Test
+    public void SortedArraysMaxShouldBeTheLastIndex() {
+        int[] array = {3,6,5,25,42,36,89};
+        int expected = array.length - 1;
+        assertEquals(expected, Utils.maxker(array));
+    }
+
+    @Test
+    public void reverseSortedArrayGivesIndex0AsMax() {
+        int[] array = {45,32,16,8,7,6,-5,-25};
+        int expected = 0;
+        assertEquals(expected, Utils.maxker(array));
+    }
+
+    @Test
+    public void IfTheMaxElementIsTheFifth_maxkerShouldGive4() {
+        int[] array = {
+                12,45,65,78,
+                25_999,
+                64,98,75,1,2
+        };
+
+        int expected = 4;
+        assertEquals(expected, Utils.maxker(array));
+    }
 }
